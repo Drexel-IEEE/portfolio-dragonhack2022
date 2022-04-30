b5fb395fe219fee2e7fd3d94c2d9537ce3c195c7
Nom Phan <np656@drexel.edu>
2022-04-29 22:13:36 -0400

    update clean

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,30 +8,6 @@ const Navbar = () => {
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
       </div>
-
-      {/* menu */}
-      <ul className='hidden md:flex'>
-        <li>
-          <Link to='home' smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to='about' smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to='skills' smooth={true} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link to='contact' smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
     </div>
   );
 };
